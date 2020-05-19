f8d8f907f5b26ecbefabd0ec575a67ed47b5dd0b
Henry Cook <henrycook@Henrys-MacBook-Air.local>
2020-05-19 18:18:23 -0400

    Full code before refactoring with hooks

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -11,10 +12,13 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
 
+  //Get first 30 default users to populate homepage
   async componentDidMount() {
     this.setState({ loading: true });
 
@@ -25,6 +29,7 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 
+  //Search for users
   searchUsers = async (text) => {
     this.setState({ loading: true });
     const res = await axios.get(
@@ -34,16 +39,39 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  //Get single github user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
+
+  //Get Users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
+  //Clear User
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  //Alert if no input in search and search button is pressed
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
 
     setTimeout(() => this.setState({ alert: null }), 3000);
   };
 
+  //The main app component, made up of each component
   render() {
-    const { users, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -68,6 +96,20 @@ class App extends Component {
                 )}
               />
               <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
